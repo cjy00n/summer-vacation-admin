c06159a520fe78522db4d2d5e3336cd518ee199f
cjy00n <ej921004@naver.com>
2024-03-28 20:29:42 +0900

    Feat : add Header component

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { ROUTE } from "../../routes/Route";
+import LogoutIcon from "../../assets/icons/LogoutIcon";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate(ROUTE.LOGIN_PAGE.link);
+  };
+
+  return (
+    <header className="bg-gray-70 px-4 flex justify-between sticky h-[8dvh] my-auto items-center">
+      <span className="font-bold text-lg">여름방학 어드민</span>
+      <button className="flex gap-2" onClick={handleLogout}>
+        로그아웃
+        <LogoutIcon />
+      </button>
+    </header>
+  );
+};
+
+export default Header;

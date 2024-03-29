5b9f231942e336f0b10e5657d38852d77000dcda
cjy00n <ej921004@naver.com>
2024-03-30 01:36:43 +0900

    Chore : add CloseIcon component

---
+++ src/assets/icons/CloseIcon.tsx
@@ -0,0 +1,16 @@
+const CloseIcon = () => (
+  <svg
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11.3527 1.84528L10.2574 0.75L5.9151 5.09228L1.57282 0.75L0.477539 1.84528L4.81982 6.18756L0.477539 10.5298L1.57282 11.6251L5.9151 7.28284L10.2574 11.6251L11.3527 10.5298L7.01038 6.18756L11.3527 1.84528Z"
+      fill="black"
+    />
+  </svg>
+);
+
+export default CloseIcon;

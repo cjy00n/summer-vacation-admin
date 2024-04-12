3d869e4062112427b8e9b1c6e27e7081765e4e41
cjy00n <ej921004@naver.com>
2024-04-12 16:40:47 +0900

    Feat : add AuthorityPage UI

--- src/pages/AuthorityPage.tsx
+++ src/pages/AuthorityPage.tsx
@@ -1,4 +1,49 @@
+import MainTitle from "../components/common/MainTitle";
+import { useGetAdmins } from "../hooks/getAdmins";
+
 const AuthorityPage = () => {
-  return <div></div>;
+  const { data: admins } = useGetAdmins();
+
+  return (
+    <div>
+      <MainTitle title="권한" />
+      <div className="w-full flex flex-col">
+        {admins && (
+          <table className="table-fixed">
+            <thead>
+              <tr>
+                <th> </th>
+                <th>#</th>
+                <th>ID</th>
+                <th>비밀번호</th>
+                <th>소유자</th>
+                <th> </th>
+              </tr>
+            </thead>
+            <tbody>
+              {admins.map((admin) => (
+                <tr key={"faq-category" + admin.id}>
+                  <td className="w-8">
+                    <input type="checkbox" />
+                  </td>
+                  <td className="w-12">{admin.id}</td>
+                  <td className="">{admin.username}</td>
+                  <td>{admin.password}</td>
+                  <td>{admin.name}</td>
+                  <td className="w-12">
+                    <button>수정</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+      <div className="flex justify-between my-3">
+        <button className="bg-gray-80 w-[14vw] rounded-md h-8">삭제</button>
+        <button className="bg-gray-80 w-[14vw] rounded-md h-8">추가</button>
+      </div>
+    </div>
+  );
 };
 export default AuthorityPage;
---
+++ src/types/Admin.ts
@@ -0,0 +1,6 @@
+export default interface Admin {
+  id: string;
+  username: string;
+  password: string;
+  name: string;
+}

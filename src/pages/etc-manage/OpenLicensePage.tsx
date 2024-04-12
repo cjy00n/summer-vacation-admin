b6dc77ce65563bd424e6e4f6e0bd95579bea6557
cjy00n <ej921004@naver.com>
2024-04-12 16:13:11 +0900

    Feat : add OpenLicensePage UI

--- src/pages/etc-manage/OpenLicensePage.tsx
+++ src/pages/etc-manage/OpenLicensePage.tsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+import MainTitle from "../../components/common/MainTitle";
+import { useGetOpenLicense } from "../../hooks/getOpenLicense";
+
 const OpenLicensePage = () => {
-  return <div></div>;
+  const { data: openLicense } = useGetOpenLicense();
+  const [inputOpenLicense, setInputOpenLicense] = useState(openLicense);
+
+  useEffect(() => {
+    if (openLicense) {
+      setInputOpenLicense(openLicense);
+    }
+  }, [openLicense]);
+
+  return (
+    <div>
+      <MainTitle title="오픈 라이선스" />
+      <textarea
+        className="bg-gray-60 w-full resize-none h-80 p-2"
+        value={inputOpenLicense}
+        onChange={(e) => setInputOpenLicense(e.target.value)}
+      />
+      <div className="flex justify-end my-2">
+        <button className="bg-gray-60 w-[15vw] h-8 rounded-md">적용</button>
+      </div>
+    </div>
+  );
 };
 export default OpenLicensePage;

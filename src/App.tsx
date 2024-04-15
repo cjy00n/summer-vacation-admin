d0a6d83ff1de9987b9e39c364a9c456c1138a93f
cjy00n <ej921004@naver.com>
2024-04-16 02:44:26 +0900

    Feat : add ToastMessageContainer import at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,43 @@ import SideBar from "./components/common/SideBar";
 import "./App.css";
 import "react-datepicker/dist/react-datepicker.css";
 import Header from "./components/common/Header";
+import ToastMessageContainer from "./components/common/ToastMessageContainer";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <Routes>
-      {ROUTE_ARR.map((el) => (
-        <Route
-          path={el.path}
-          element={
-            <div className="flex-col">
-              {el.haveHeader === false && el.haveSideBar === false ? (
-                <div className="w-full h-dvh">{el.element}</div>
-              ) : (
-                <>
-                  <Header />
-                  <div className="flex h-[92dvh]">
-                    <SideBar />
-                    <div className="w-[75%] mx-auto px-12 py-6">
-                      {el.element}
+    <RecoilRoot>
+      <Routes>
+        {ROUTE_ARR.map((el) => (
+          <Route
+            path={el.path}
+            element={
+              <div className="flex-col">
+                <ToastMessageContainer />
+                {el.haveHeader === false && el.haveSideBar === false ? (
+                  <div className="w-full h-dvh">{el.element}</div>
+                ) : (
+                  <>
+                    <Header />
+                    <div className="flex h-[92dvh]">
+                      <SideBar />
+                      <div className="w-[75%] mx-auto px-12 py-6">
+                        {el.element}
+                      </div>
                     </div>
-                  </div>
-                </>
-              )}
-            </div>
-          }
-          key={el.path}
+                  </>
+                )}
+              </div>
+            }
+            key={el.path}
+          />
+        ))}
+        <Route
+          path="/"
+          element={<Navigate replace to={ROUTE.USERS_PAGE.link} />}
         />
-      ))}
-
-      <Route
-        path="/"
-        element={<Navigate replace to={ROUTE.USERS_PAGE.link} />}
-      />
-    </Routes>
+      </Routes>
+    </RecoilRoot>
   );
 }
 

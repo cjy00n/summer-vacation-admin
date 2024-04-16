32fe78a1a801365b8bb941abdd7ce0c490b7baef
cjy00n <ej921004@naver.com>
2024-04-16 19:06:56 +0900

    Chore : modify Diary type

--- src/types/Diary.ts
+++ src/types/Diary.ts
@@ -1,11 +1,19 @@
+export type Emotion = "괜찮아요" | "좋아요" | "기뻐요" | "화나요" | "슬퍼요";
+export type Weather = "맑음" | "비" | "눈" | "흐림" | "천둥";
+
 export default interface Diary {
   id: string;
+  title: string;
+  contents: string;
+  date: Date;
+  emotion: Emotion;
+  weather: Weather;
+  isPublic: 0 | 1;
+  imageUrl: string;
+  createdAt: Date;
   nickname: string;
   kakaoId: string;
   warning: boolean;
   reportNum: number;
-  contents: string;
-  date: Date;
   image: string;
-  title: string;
 }

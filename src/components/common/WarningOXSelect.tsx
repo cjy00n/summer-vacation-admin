57f2dda4afd3df3a5369d505ad490cf2851f9a63
cjy00n <ej921004@naver.com>
2024-04-17 00:37:25 +0900

    Feat : add props to WarningOXSelect

--- src/components/common/WarningOXSelect.tsx
+++ src/components/common/WarningOXSelect.tsx
@@ -1,23 +1,39 @@
-const WarningOXSelect = () => {
+import { Warning } from "../../types/DiarySearchQuery";
+
+interface WarningOXSelectProps {
+  warning: Warning;
+  setWarning: (warning: Warning) => void;
+}
+
+const WarningOXSelect = ({ warning, setWarning }: WarningOXSelectProps) => {
+  const handleWarning = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setWarning(event.target.value as Warning);
+  };
+
   return (
-    <form className="flex h-8 items-center w-[15vw]">
-      <div className="w-[8vw]">
+    <form className="flex h-8 items-center w-middle justify-between">
+      <div className="w-1/2">
         <input
           id="report-X"
           type="radio"
-          value={"X"}
           name="X"
+          value={"X"}
           className="mx-1"
+          checked={warning === "X"}
+          onChange={handleWarning}
         />
         <label htmlFor="report-X">X</label>
       </div>
-      <div className="w-[8vw]">
+      <div className="w-1/2 ">
         <input
           id="report-O"
           type="radio"
-          value={"O"}
           name="O"
+          value={"O"}
           className="mx-1"
+          checked={warning === "O"}
+          onChange={handleWarning}
         />
         <label htmlFor="report-O">O</label>
       </div>

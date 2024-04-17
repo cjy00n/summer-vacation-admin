e5b4e8c8089e3f1137d69710d9d174c5c5eed55a
cjy00n <ej921004@naver.com>
2024-04-18 01:17:23 +0900

    Feat : split pagination component

--- src/components/users/UsersResult.tsx
+++ src/components/users/UsersResult.tsx
@@ -1,19 +1,12 @@
 import { format } from "date-fns";
 import User from "../../types/User";
 import { ko } from "date-fns/locale";
-import { useState } from "react";
 
 interface UsersResultProps {
   users: User[];
 }
 
 const UsersResult = ({ users }: UsersResultProps) => {
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handlePage = (page: number) => {
-    setCurrentPage(page);
-  };
-
   return (
     <div className="w-full flex flex-col">
       <table>
@@ -55,23 +48,6 @@ const UsersResult = ({ users }: UsersResultProps) => {
       <button className="bg-gray-60 py-1 text-sm rounded-sm my-2 mx-1 w-20">
         삭제
       </button>
-      <div className="py-8 gap-5 flex w-2/3 mx-auto justify-center ">
-        <button>&lt;&lt;</button>
-        <button>&lt;</button>
-        {Array(6)
-          .fill(0)
-          .map((_, i) => (
-            <button
-              key={"users-page-" + i}
-              onClick={() => handlePage(i + 1)}
-              className={currentPage === i + 1 ? "text-black" : "text-gray-50"}
-            >
-              {i + 1}
-            </button>
-          ))}
-        <button>&gt;</button>
-        <button>&gt;&gt;</button>
-      </div>
     </div>
   );
 };

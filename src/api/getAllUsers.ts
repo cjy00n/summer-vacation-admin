045619414a71f06ca4251a1e7e21c20996845af2
cjy00n <ej921004@naver.com>
2024-04-18 01:17:47 +0900

    Feat : add getPageMeta

--- src/api/getAllUsers.ts
+++ src/api/getAllUsers.ts
@@ -1,19 +1,22 @@
 import { useQuery } from "react-query";
 import { authInstance } from ".";
 import User from "../types/User";
+import PageMeta from "../types/PageMeta";
 
-export const getAllUsers = async () => {
+export const getAllUsers = async (page: number) => {
   try {
-    const response = await authInstance.get<{ data: User[] }>("user/info");
+    const response = await authInstance.get<{ data: User[]; meta: PageMeta }>(
+      "user/info?page=" + page
+    );
 
-    if (response.data.data) {
-      return response.data.data;
+    if (response.data) {
+      return { data: response.data.data, pageMeta: response.data.meta };
     }
   } catch (e) {
     console.error(e);
   }
 };
 
-export function useGetAllUsers() {
-  return useQuery(["getAllUsers"], () => getAllUsers());
+export function useGetAllUsers(page: number) {
+  return useQuery(["getAllUsers" + page], () => getAllUsers(page));
 }
---
+++ src/types/PageMeta.ts
@@ -0,0 +1,5 @@
+export default interface PageMeta {
+  total: number;
+  page: number;
+  last_page: number;
+}

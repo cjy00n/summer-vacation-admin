f09b1fcc7c8f96f3c54548aa32267e206efe4b2a
cjy00n <ej921004@naver.com>
2024-04-18 23:31:14 +0900

    Refact : move CustomModal

--- src/components/Feed/BannerSection.tsx
+++ src/components/Feed/BannerSection.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ConfirmModal from "../common/ConfirmModal";
+import ConfirmModal from "../Modal/ConfirmModal";
 
 const BannerSection = () => {
   const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);

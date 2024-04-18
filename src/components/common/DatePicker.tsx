67441e57bd3d995c732974d34484b81c658d4a18
cjy00n <ej921004@naver.com>
2024-04-18 23:28:17 +0900

    Chore : add DatePick at FeedPage

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -4,17 +4,27 @@ import ReactDatePicker from "react-datepicker";
 interface DatePickerProps {
   selected: Date | null;
   onChange: (date: Date) => void;
-  type?: "date" | "year";
+  type?: "date" | "year" | "month";
+  size?: "default" | "short";
 }
 
-const DatePicker = ({ selected, onChange, type = "date" }: DatePickerProps) => {
+const DatePicker = ({
+  selected,
+  onChange,
+  type = "date",
+  size = "default",
+}: DatePickerProps) => {
   return (
     <ReactDatePicker
+      className={size === "short" ? "w-[8vw]" : "w-middle"}
       locale={ko}
       selected={selected}
       onChange={onChange}
       showYearPicker={type == "year"}
-      dateFormat={type == "year" ? "yyyy" : "yyyy-MM-dd"}
+      showMonthYearPicker={type === "month"}
+      dateFormat={`${
+        type == "year" ? "yyyy" : type == "month" ? "MM" : "yyyy-MM-dd"
+      }`}
       yearItemNumber={10}
     />
   );
--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -6,35 +6,61 @@ import SubTitle from "../components/common/SubTitle";
 import { useGetContest } from "../api/getContest";
 import Warning from "../components/Warning";
 import Loading from "../components/Loading";
+import DatePicker from "../components/common/DatePicker";
+import { useState } from "react";
+import { ContestPeriod } from "../types/ContestPeriod";
 
 const FeedPage = () => {
-  const { data: contestList, isLoading: contentLoading } = useGetContest({
-    year: 2024,
-    month: 3,
-    week: 2,
+  const today = new Date();
+
+  const [selectedPeriod, setSelectedPeriod] = useState({
+    year: today.getFullYear(),
+    month: today.getMonth(),
+    week: (today.getDay() % 7) + 1,
   });
+
+  const updateField = <K extends keyof ContestPeriod>(
+    field: K,
+    value: ContestPeriod[K]
+  ) => {
+    setSelectedPeriod({
+      ...selectedPeriod,
+      [field]: value,
+    });
+  };
+
+  const { data: contestList, isLoading: contentLoading } =
+    useGetContest(selectedPeriod);
 
   return (
     <div className="flex flex-col">
       <MainTitle title="홈" />
       <SubTitle title="배너" />
       <SubTitle title="이미지 업로드" />
-
       <BannerSection />
       <MainTitle title="피드" />
       <div className="flex items-center">
         <SubTitle title="사생대회" />
         <div className="flex gap-2 mx-8">
-          <button className="border border-gray-80 border-solid py-1 w-28 flex justify-center gap-2 items-center px-2">
-            2024년 <ArrowIcon />
-          </button>
-          <button className="border border-gray-80 border-solid py-1 w-28 flex justify-center gap-2 items-center px-2">
-            03월 <ArrowIcon />
-          </button>
-          <button className="border border-gray-80 border-solid py-1 w-28 flex justify-center gap-2 items-center px-2">
+          <DatePicker
+            type="year"
+            size="short"
+            selected={new Date(`${selectedPeriod.year}`)}
+            onChange={(date: Date) => updateField("year", date.getFullYear())}
+          />
+
+          <DatePicker
+            type="month"
+            size="short"
+            selected={new Date(selectedPeriod.year, selectedPeriod.month)}
+            onChange={(date: Date) => updateField("month", date.getMonth())}
+          />
+          <button className="border border-gray-80 border-solid py-1 w-[8vw] flex justify-center gap-2 items-center px-2">
             1주 <ArrowIcon />
           </button>
-          <button className="bg-black text-white rounded-md px-6">검색</button>
+          <button className="bg-black text-white rounded-md w-short">
+            검색
+          </button>
         </div>
       </div>
       <MainTitle title="수상목록" />
